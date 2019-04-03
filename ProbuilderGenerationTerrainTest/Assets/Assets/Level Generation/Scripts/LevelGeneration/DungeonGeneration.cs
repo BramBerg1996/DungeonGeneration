85deb842d3e6548e51b1d0826fdbb6e5dd73b3c7
Tim Hendriksen <tim_hendriksen@outlook.com>
2019-04-03 20:23:01 +0200

    Added multiple levels.

--- ProbuilderGenerationTerrainTest/Assets/Assets/Level Generation/Scripts/LevelGeneration/DungeonGeneration.cs
+++ ProbuilderGenerationTerrainTest/Assets/Assets/Level Generation/Scripts/LevelGeneration/DungeonGeneration.cs
@@ -2,17 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
+using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
 
 public class DungeonGeneration : MonoBehaviour
 {
-    public int xBounds, zBounds;
+    /**
+     * Out of bounds on Z axes goes wrong, don't know why.
+     * Sometimes the y goes out of bounds when its less then 0.
+     */
+    public int xBounds, zBounds, yBounds;
 
     public int ForwardChance;
     public int BackwardChance;
     public int LeftChance;
     public int RightChance;
+    public int UpChance;
+    public int DownChance;
 
     public bool UseSeed = false;
     public int seed;
@@ -22,14 +30,16 @@ public class DungeonGeneration : MonoBehaviour
     public GameObject CorridorPiece;
     public GameObject CornerPiece;
     public GameObject StartEndPiece;
+    public GameObject UpPiece;
+    public GameObject DownPiece;
     public GameObject player;
 
     private static int PIECE_SIZE = 10;
 
-    private int[,] dungeonLayout;
+    private int[,,] dungeonLayout;
     private GameObject parent;
 
-    private int x, z;
+    private int x, z, y;
     private Directions nextDirection = Directions.NONE;
     private Directions prevDirection = 0;
 
@@ -43,7 +53,7 @@ public class DungeonGeneration : MonoBehaviour
     private bool generatingLevel = true;
     private bool startPlaced = false;
 
-    private enum Directions { NONE, FORWARD, RIGHT, LEFT, BACK, BLOCKED, UP, DOWN,};
+    private enum Directions { NONE, FORWARD, RIGHT, LEFT, BACK, BLOCKED, UP, DOWN, };
 
     void Start()
     {
@@ -52,7 +62,7 @@ public class DungeonGeneration : MonoBehaviour
             Debug.LogError("Bounds are not set properly");
         }
 
-        if (ForwardChance + BackwardChance + LeftChance + RightChance != 100)
+        if (ForwardChance + BackwardChance + LeftChance + RightChance + UpChance + DownChance != 100)
         {
             Debug.LogError("Total chance not 100% " + (ForwardChance + BackwardChance + LeftChance + RightChance));//change to end game.
         }
@@ -60,7 +70,7 @@ public class DungeonGeneration : MonoBehaviour
         parent = new GameObject();
         parent.name = "Dungeon";
 
-        dungeonLayout = new int[xBounds, zBounds];
+        dungeonLayout = new int[xBounds, zBounds, yBounds];
 
         if (UseSeed)
         {
@@ -68,7 +78,7 @@ public class DungeonGeneration : MonoBehaviour
         }
         else
         {
-            seed = Random.Range(0,max: int.MaxValue);
+            seed = Random.Range(0, max: int.MaxValue);
             Random.InitState(seed);
         }
 
@@ -79,6 +89,7 @@ public class DungeonGeneration : MonoBehaviour
 
         x = Random.Range(0, xBounds);
         z = Random.Range(0, zBounds / 2);
+        y = Random.Range(0, yBounds);
 
         // create start position
         createPart();
@@ -88,7 +99,7 @@ public class DungeonGeneration : MonoBehaviour
     void Update()
     {
         if (generatingLevel)
-        {        
+        {
             //generate piece of the dungeon every update
             generateDungeon();
         }
@@ -121,6 +132,12 @@ public class DungeonGeneration : MonoBehaviour
                 break;
             case Directions.BACK:
                 z--; ;
+                break;
+            case Directions.UP:
+                y++;
+                break;
+            case Directions.DOWN:
+                y--;
                 break;
             default:
                 break;
@@ -158,9 +175,21 @@ public class DungeonGeneration : MonoBehaviour
             }
 
             //left
-            if (chance >= ForwardChance + BackwardChance + RightChance && chance <= 100)
+            if (chance >= ForwardChance + BackwardChance + RightChance && chance <= ForwardChance + BackwardChance + RightChance + LeftChance)
             {
                 return Directions.LEFT;
+            }
+
+            //up
+            if (chance >= ForwardChance + BackwardChance + RightChance + LeftChance && chance <= ForwardChance + BackwardChance + RightChance + LeftChance + UpChance)
+            {
+                return Directions.UP;
+            }
+
+            //down
+            if (chance >= ForwardChance + BackwardChance + RightChance + LeftChance + UpChance && chance <= ForwardChance + BackwardChance + RightChance + LeftChance + UpChance + DownChance)
+            {
+                return Directions.DOWN;
             }
         }
 
@@ -173,22 +202,23 @@ public class DungeonGeneration : MonoBehaviour
     {
         Directions dir = GetChanceBasedDirections();
         bool notBlocked = false;
-        
+
 
 
         // can forward
-        if (z < zBounds - 1 && dungeonLayout[x, z + 1] == 0)
+        if (z < zBounds - 1 && dungeonLayout[x, z + 1, y] == 0)
         {
             if (dir == Directions.FORWARD)
             {
                 return Directions.FORWARD;
-            }else if (ForwardChance > 0)
+            }
+            else if (ForwardChance > 0)
             {
                 notBlocked = true;
             }
         }
         // can back
-        if (z - 1 > 0 && dungeonLayout[x, z - 1] == 0)
+        if (z - 1 > 0 && dungeonLayout[x, z - 1, y] == 0)
         {
             if (dir == Directions.BACK)
             {
@@ -200,7 +230,7 @@ public class DungeonGeneration : MonoBehaviour
             }
         }
         // can right
-        if (x < xBounds - 1 && dungeonLayout[x + 1, z] == 0)
+        if (x < xBounds - 1 && dungeonLayout[x + 1, z, y] == 0)
         {
             if (dir == Directions.RIGHT)
             {
@@ -212,18 +242,45 @@ public class DungeonGeneration : MonoBehaviour
             }
         }
         // can left
-        if (x > 0 && dungeonLayout[x - 1, z] == 0)
+        if (x > 0 && dungeonLayout[x - 1, z, y] == 0)
         {
             if (dir == Directions.LEFT)
             {
                 return Directions.LEFT;
-            }else if (LeftChance > 0)
+            }
+            else if (LeftChance > 0)
             {
                 notBlocked = true;
             }
         }
 
-        if (notBlocked && dir != Directions.BLOCKED) {
+        // can up
+        if (x > 0 && dungeonLayout[x, z, y + 1] == 0 && lastPiece != null && lastPiece.name != "START" && lastPiece.name != "UP")
+        {
+            if (dir == Directions.UP)
+            {
+                return Directions.UP;
+            }
+            else if (UpChance > 0)
+            {
+                notBlocked = true;
+            }
+        }
+        //can down
+        if (x > 0 && dungeonLayout[x, z, y - 1] == 0 && lastPiece != null && lastPiece.name != "START" && lastPiece.name != "DOWN")
+        {
+            if (dir == Directions.DOWN)
+            {
+                return Directions.DOWN;
+            }
+            else if (DownChance > 0)
+            {
+                notBlocked = true;
+            }
+        }
+
+        if (notBlocked && dir != Directions.BLOCKED)
+        {
             return getNextDirection();
         }
 
@@ -233,7 +290,7 @@ public class DungeonGeneration : MonoBehaviour
     //if out of bounds of the dungeon generation area return true;
     bool outOfBounds()
     {
-        return x == xBounds - 1 || z == zBounds - 1 || x < 0 || z < 0;
+        return x == xBounds - 1 || z == zBounds - 1 || y == yBounds || y < 0 || x < 0 || z < 0;
     }
 
     void createPart()
@@ -243,15 +300,20 @@ public class DungeonGeneration : MonoBehaviour
 
         GameObject piece = null;
 
-        // place end
+        // place start or end
         if (placeDirection == Directions.NONE || placeDirection == Directions.BLOCKED)
         {
-            piece = Instantiate(StartEndPiece, new Vector3(x * PIECE_SIZE, 0, z * PIECE_SIZE), Quaternion.identity);
+            piece = Instantiate(StartEndPiece, new Vector3(x * PIECE_SIZE, y * 5, z * PIECE_SIZE), Quaternion.identity);
             //spawning start
             if (!startPlaced)
             {
                 startPlaced = true;
-                //spawnPlayer(piece.transform.position);
+                piece.name = "START";
+                Component[] childs = piece.GetComponentsInChildren<Renderer>();
+                foreach (var child in childs)
+                {
+                    child.GetComponent<Renderer>().material.color = Color.green;
+                }
                 if (nextDirection == Directions.RIGHT)
                 {
                     currentRotation = 180;
@@ -268,12 +330,18 @@ public class DungeonGeneration : MonoBehaviour
                 {
                     currentRotation = 270;
                 }
-
+                spawnPlayer(piece.transform.position + new Vector3(0, 10, 0));
             }
             //spawning end
             else
             {
-                if (lastPiece.name == "corner")
+                piece.name = "END";
+                Component[] childs = piece.GetComponentsInChildren<Renderer>();
+                foreach (var child in childs)
+                {
+                    child.GetComponent<Renderer>().material.color = Color.red;
+                }
+                if (lastPiece.name == "CORNER")
                 {
                     if (lastPiece.transform.rotation.y >= 89 && lastPiece.transform.rotation.y <= 91)
                     {
@@ -313,8 +381,8 @@ public class DungeonGeneration : MonoBehaviour
         }
         else if (mustPlaceCorner(placeDirection))
         {
-            piece = Instantiate(CornerPiece, new Vector3(x * PIECE_SIZE, 0, z * PIECE_SIZE), Quaternion.identity);
-            piece.name = "corner";
+            piece = Instantiate(CornerPiece, new Vector3(x * PIECE_SIZE, y * 5, z * PIECE_SIZE), Quaternion.identity);
+            piece.name = "CORNER";
 
             if (placeDirection == Directions.FORWARD && nextDirection == Directions.RIGHT ||
                 placeDirection == Directions.LEFT && nextDirection == Directions.BACK)
@@ -338,9 +406,70 @@ public class DungeonGeneration : MonoBehaviour
                 currentRotation = 0;
             }
         }
+        else if (nextDirection == Directions.UP || nextDirection == Directions.DOWN)
+        {
+            if (nextDirection == Directions.UP)
+            {
+                piece = Instantiate(UpPiece, new Vector3(x * PIECE_SIZE, y * 5, z * PIECE_SIZE), Quaternion.identity);
+                piece.name = "UP";
+            }
+            else
+            {
+                piece = Instantiate(DownPiece, new Vector3(x * PIECE_SIZE, y * 5, z * PIECE_SIZE), Quaternion.identity);
+                piece.name = "DOWN";
+            }
+
+            if (placeDirection == Directions.RIGHT)
+            {
+                currentRotation = 0;
+            }
+            else if (placeDirection == Directions.LEFT)
+            {
+                currentRotation = 180;
+            }
+            else if (placeDirection == Directions.FORWARD)
+            {
+                currentRotation = 270;
+            }
+            else
+            {
+                currentRotation = 90;
+            }
+        }
+        else if (placeDirection == Directions.DOWN || placeDirection == Directions.UP)
+        {
+            if (placeDirection == Directions.DOWN)
+            {
+                piece = Instantiate(UpPiece, new Vector3(x * PIECE_SIZE, y * 5, z * PIECE_SIZE), Quaternion.identity);
+                piece.name = "UP";
+            }
+            else
+            {
+                piece = Instantiate(DownPiece, new Vector3(x * PIECE_SIZE, y * 5, z * PIECE_SIZE), Quaternion.identity);
+                piece.name = "DOWN";
+            }
+
+            if (nextDirection == Directions.BACK)
+            {
+                currentRotation = 270;
+            }
+            else if (nextDirection == Directions.RIGHT)
+            {
+                currentRotation = 180;
+            }
+            else if (nextDirection == Directions.FORWARD)
+            {
+                currentRotation = 90;
+            }
+            else
+            {
+                currentRotation = 0;
+            }
+        }
         else
         {
-            piece = Instantiate(CorridorPiece, new Vector3(x * PIECE_SIZE, 0, z * PIECE_SIZE), Quaternion.identity);
+            piece = Instantiate(CorridorPiece, new Vector3(x * PIECE_SIZE, y * 5, z * PIECE_SIZE), Quaternion.identity);
+            piece.name = "CORRIDOR";
             if (placeDirection == Directions.RIGHT || placeDirection == Directions.LEFT)
             {
                 currentRotation = 0;
@@ -351,7 +480,7 @@ public class DungeonGeneration : MonoBehaviour
             }
         }
         piece.transform.Rotate(0, currentRotation, 0);
-        dungeonLayout[x, z] = 1;
+        dungeonLayout[x, z, y] = 1;
         lastPos = new Vector3(x, 0, z);
         prevDirection = placeDirection;
 
@@ -371,16 +500,16 @@ public class DungeonGeneration : MonoBehaviour
 
         if (placeDirection != Directions.NONE)
         {
-            if (lastPos.x != getNextPos(nextDirection).x && lastPos.z != getNextPos(nextDirection).z)
+            if (lastPos.x != getNextPos().x && lastPos.z != getNextPos().z)
             {
                 return true;
             }
         }
         return false;
     }
-    Vector3 getNextPos(Directions nextDir)
+    Vector3 getNextPos()
     {
-        Vector3 nextPos = new Vector3(x, 0, z);
+        Vector3 nextPos = new Vector3(x, y, z);
 
         // forward
         if (nextDirection == Directions.FORWARD)
@@ -397,10 +526,20 @@ public class DungeonGeneration : MonoBehaviour
         {
             nextPos.x--;
         }
-        //down
+        //back
         if (nextDirection == Directions.BACK)
         {
             nextPos.z--;
+        }
+        //down
+        if (nextDirection == Directions.UP)
+        {
+            nextPos.y--;
+        }
+        //up
+        if (nextDirection == Directions.UP)
+        {
+            nextPos.y++;
         }
 
         return nextPos;
@@ -412,4 +551,38 @@ public class DungeonGeneration : MonoBehaviour
     }
 }
 
+[CustomEditor(typeof(DungeonGeneration))]
+public class DungeonGenerationEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var dg = target as DungeonGeneration;
+        dg.UseSeed = GUILayout.Toggle(dg.UseSeed, "Use Seed");
 
+        if (dg.UseSeed)
+        {
+            dg.seed = EditorGUILayout.IntField("Seed", dg.seed);
+        }
+
+        dg.xBounds = EditorGUILayout.IntField("X Boundary", dg.xBounds);
+        dg.yBounds = EditorGUILayout.IntField("Y Boundary", dg.yBounds);
+        dg.zBounds = EditorGUILayout.IntField("Z Boundary", dg.zBounds);
+
+        dg.ForwardChance = EditorGUILayout.IntField("Forward Chance", dg.ForwardChance);
+        dg.BackwardChance = EditorGUILayout.IntField("Backward Chance", dg.BackwardChance);
+        dg.LeftChance = EditorGUILayout.IntField("Left Chance", dg.LeftChance);
+        dg.RightChance = EditorGUILayout.IntField("Right Chance", dg.RightChance);
+        dg.DownChance = EditorGUILayout.IntField("Down Chance", dg.DownChance);
+        dg.UpChance = EditorGUILayout.IntField("Up Chance", dg.UpChance);
+
+
+        dg.AmoutOfSteps = EditorGUILayout.IntField("Amount of Steps", dg.AmoutOfSteps);
+
+        dg.CorridorPiece = (GameObject)EditorGUILayout.ObjectField(dg.CorridorPiece, typeof(Object), true);
+        dg.CornerPiece = (GameObject)EditorGUILayout.ObjectField(dg.CornerPiece, typeof(Object), true);
+        dg.StartEndPiece = (GameObject)EditorGUILayout.ObjectField(dg.StartEndPiece, typeof(Object), true);
+        dg.UpPiece = (GameObject)EditorGUILayout.ObjectField(dg.UpPiece, typeof(Object), true);
+        dg.DownPiece = (GameObject)EditorGUILayout.ObjectField(dg.DownPiece, typeof(Object), true);
+        dg.player = (GameObject)EditorGUILayout.ObjectField(dg.player, typeof(Object), true);
+    }
+}
